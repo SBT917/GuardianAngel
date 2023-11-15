48bfae57c2aab14259973f9dac5ab4ea12f6239c
CI0403 <22ci0403@jec.ac.jp>
2023-11-15 13:57:12 +0900

    リザルトの階級とコメントをjsonファイルから読み込むよう変更

---
+++ Assets/Iino/Scripts/RankManager.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Rank
+{
+    public string rank;
+    public int minScore;
+    public string comment;
+}
+
+[Serializable]
+public class RankList
+{
+    public List<Rank> ranks;
+}
+
+public class RankManager : MonoBehaviour
+{
+    public RankList RankList;
+
+    public static RankManager instance;
+
+    private void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
+
+    }
+
+    private void Start()
+    {
+        LoadRanks();
+    }
+
+
+    void LoadRanks()
+    {
+        TextAsset ranksJson = Resources.Load<TextAsset>("Ranks");
+        RankList = JsonUtility.FromJson<RankList>(ranksJson.text);
+    }
+
+    public Rank GetRank(int score)
+    {
+        foreach (var rank in RankList.ranks)
+        {
+            if(score >= rank.minScore)
+            {
+                return rank;
+            }
+        }
+        return null;
+    }
+}
--- Assets/Iino/Scripts/ResultManager.cs
+++ Assets/Iino/Scripts/ResultManager.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class ResultManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text peopleHelpedNumText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text angelRank;
+    [SerializeField]
+    private Text godsComment;
+
+
+    //デバッグ用　スコアを直接打ち込む
+    [SerializeField]
+    private int peopleHelped;
+    [SerializeField]
+    private int score;
+
+    private Rank rank;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)) 
+        {
+            LoadRank();
+        }
+    }
+
+    void LoadRank()
+    {
+        rank = RankManager.instance.GetRank(score);
+        UpdateResultText(rank);
+    }
+
+    void UpdateResultText(Rank rank)
+    {
+        if (rank == null)
+        {
+            Debug.LogError("Rank object is null.");
+            return;
+        }
+
+        peopleHelpedNumText.text = "助けた人：" + peopleHelped.ToString() + "人";
+        scoreText.text = "最終スコア：" + score.ToString();
+        angelRank.text = rank.rank.ToString();
+        godsComment.text = rank.comment.ToString();
     }
 }
