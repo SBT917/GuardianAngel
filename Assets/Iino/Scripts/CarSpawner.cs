b95e44bfe537ac41b0d1a259bcca0113f3a04e81
CI0403 <22ci0403@jec.ac.jp>
2023-12-14 11:34:42 +0900

    生成される車にバリエーションを追加し、ランダムに生成されるよう変更 車が巡回ポイントを一定回数周った後、元の場所に変えるよう変更 NavMeshが一部の建物を無視していたため焼き直し

--- Assets/Iino/Scripts/CarSpawner.cs
+++ Assets/Iino/Scripts/CarSpawner.cs
@@ -4,10 +4,20 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class CarManager : Singleton<CarManager>
+
+[CreateAssetMenu(fileName = "CarData",menuName ="ScriptableObjects/CarData",order =1)]
+public class CarData : ScriptableObject
+{
+    public GameObject CarPrefab;
+    public float weight;
+}
+
+
+
+public class CarSpawner : Singleton<CarSpawner>
 {
     [SerializeField]
-    private List<GameObject> cars;
+    private List<CarData> cars;
 
     [SerializeField]
     private Vector3 carSpawnPoint;
@@ -28,8 +38,7 @@ public class CarManager : Singleton<CarManager>
     // Start is called before the first frame update
     void Start()
     {
-        var initialCarNum = GameObject.FindGameObjectsWithTag("Car");
-        carCount = initialCarNum.Length;
+        InitSpawnCar();
     }
 
     private void Update()
@@ -38,6 +47,12 @@ public class CarManager : Singleton<CarManager>
         {
             SpawnCar();
         }
+    }
+
+    private void InitSpawnCar()
+    {
+        carGenQueue = CarMaxNum;
+        StartCoroutine(GenerateCarWithDelay());
     }
 
     public void SpawnCar()
@@ -69,12 +84,39 @@ public class CarManager : Singleton<CarManager>
                 canSpawn = false; // 生成処理開始時にフラグをオフにする
                 yield return new WaitForSeconds(3f); // 3秒待つ
                 carCount++;
-                var newCar = Instantiate(cars[Random.Range(0, cars.Count)], carSpawnPoint, Quaternion.identity);
+
+
+                var newCar = Instantiate(SelectCar(), carSpawnPoint, Quaternion.identity);
                 var carPatrol = newCar.GetComponent<CarPatrol>();
                 canSpawn = true; // 生成が完了したらフラグをオンにする
                 carGenQueue--;
             }
         }
         generatingCar= false;
+    }
+
+
+    private GameObject SelectCar()
+    {
+        float totalWeight = 0;
+        foreach (CarData carData in cars)
+        {
+            totalWeight += carData.weight;
+        }
+
+        float randomNum = Random.Range(0f, totalWeight);
+        foreach (CarData carData in cars)
+        {
+            if (randomNum < carData.weight)
+            {
+                return carData.CarPrefab;
+            }
+            else
+            {
+                randomNum -= carData.weight;
+            }
+        }
+        
+        return cars[0].CarPrefab;
     }
 }
