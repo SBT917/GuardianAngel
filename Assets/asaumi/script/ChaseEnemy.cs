67343b786945b3c61095f41ddc3a1acd5de95418
22ci0401 <22ci0401@jec.ac.jp>
2024-01-11 11:09:44 +0900

    zombie作成中

--- Assets/asaumi/script/ChaseEnemy.cs
+++ Assets/asaumi/script/ChaseEnemy.cs
@@ -5,23 +5,44 @@ using UnityEngine.AI;
 
 public class EnemyC : MonoBehaviour
 {
-    [SerializeField]
-    [Tooltip("追いかける対象")]
-    private GameObject player;
-
     private NavMeshAgent navMeshAgent;
-
-    // Start is called before the first frame update
+    GameObject targetCarHuman;
     void Start()
     {
-        // NavMeshAgentを保持しておく
         navMeshAgent = GetComponent<NavMeshAgent>();
+
+        // 最初の追従対象を設定
+        FindNewTarget();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // プレイヤーを目指して進む
-        navMeshAgent.destination = player.transform.position;
+        if(targetCarHuman != null)
+        {
+            navMeshAgent.SetDestination(targetCarHuman.transform.position);
+        }
+        else
+        {
+            FindNewTarget();
+        }
+
+        // 追従対象の位置を設定
+        if (navMeshAgent.remainingDistance < 0.1f)
+        {
+
+            // ターゲットに到達したら新しいターゲットを検索
+            FindNewTarget();
+        }
+    }
+
+    void FindNewTarget()
+    {
+        // CarHumanオブジェクトを検索し、ランダムに一つ選択
+        GameObject[] carHumans = GameObject.FindGameObjectsWithTag("CarHuman");
+        if (carHumans.Length > 0)
+        {
+            targetCarHuman = carHumans[Random.Range(0, carHumans.Length)];
+            navMeshAgent.SetDestination(targetCarHuman.transform.position);
+        }
     }
 }
