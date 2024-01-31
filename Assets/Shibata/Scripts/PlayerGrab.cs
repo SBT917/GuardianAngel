82e01e25de8263845476d929896f7153775548d1
SBT917 <22ci0421@jec.ac.jp>
2024-01-31 16:05:12 +0900

    ビルド時のエラーを修正

--- Assets/Shibata/Scripts/PlayerGrab.cs
+++ Assets/Shibata/Scripts/PlayerGrab.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityFx.Outline;
-using static UnityEditor.PlayerSettings;
 
 public class PlayerGrab : MonoBehaviour
 {
